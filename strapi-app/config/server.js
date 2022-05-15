ca264a301a8488d9a1a73d03551e4d705b55dedd
m5151128 <nagayama@thedott.io>
2022-05-15 19:24:17 +0900

    一旦これで動く

--- strapi-app/config/server.js
+++ strapi-app/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'http://localhost:8080/api'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'cc0d3a3200fcb5651ec782a7a1ceb2dd'),
--- strapi-app/server.js
+++ strapi-app/server.js
@@ -1,2 +1,2 @@
-const strapi = require('@strapi/strapi');
+const strapi = require('strapi');
 strapi().start();
